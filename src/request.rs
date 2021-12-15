0256fa448fbe6b7127829eab67b8edd31e0d6cf9
window_land <windowlandns@gmail.com>
2021-12-15 13:56:58 -0600

    made some tweaks to the trait, added, and added reqwest impl

--- src/request.rs
+++ src/request.rs
@@ -6,8 +6,14 @@
 
 /// This trait is designed for use by http clients.
 pub trait Client {
-  /// The builder type.
+  ///Error returned if the request fails.
+  type Error;
+  ///Future returned by send.
+  type Output: Future<Output = Result<Bytes, Self::Error>> + Unpin + Send;
   type Builder: RequestBuilder;
+  ///Sends the request, returning a future that resolves to the output.
+  /// The server returning a 4XX or a 5XX response code should be considered an error.
+  fn send(&self, request: <Self::Builder as RequestBuilder>::Built) -> Self::Output;
   ///Returns a configurable builder that represents a get request to the url.
   /// This function should not panic, instead errors should be reported when
   /// the request is built.
@@ -17,15 +23,15 @@ pub trait Client {
 ///Trait that represents a builder for a request.
 pub trait RequestBuilder {
   /// Type of the built request
-  type Built: Request;
+  type Built;
   /// Error type returned when the build fails.
   type BuildError;
   /// Adds a header value pair to the request. Should not panic, instead errors should
   /// be reported when the request is built.
-  fn add_header(&mut self, name: &str, value: &str);
+  fn add_header(self, name: &str, value: &str) -> Self;
   /// Adds serializable values to the query string. Should not panic, instead errors
   /// be reported when the request is built.
-  fn add_queries<S: Serialize>(&mut self, query: S);
+  fn add_queries<S: Serialize>(self, query: &S) -> Self;
   /// Builds the request, returning an error if something went wrong when building.
   fn build(self) -> Result<Self::Built, Self::BuildError>;
 }
@@ -33,13 +39,39 @@ use bytes::Bytes;
 use serde::Serialize;
 use std::future::Future;
 
-///Trait respresenting a sendable http request.
-pub trait Request {
-  ///Error returned if the request fails.
-  type Error;
-  ///Future returned by send.
-  type Output: Future<Output = Result<Bytes, Self::Error>>;
-  ///Sends the request, returning a future that resolves to the output.
-  /// The server returning a 4XX or a 5XX response code should be considered an error.
-  fn send(self) -> Self::Output;
+#[cfg(feature = "reqwest")]
+mod reqwest {
+  use ::reqwest::Client;
+  use ::reqwest::Error;
+  use ::reqwest::Request;
+  use ::reqwest::RequestBuilder;
+  use bytes::Bytes;
+  use futures::TryFutureExt;
+  use serde::Serialize;
+  use std::future::Future;
+  use std::pin::Pin;
+  impl super::Client for Client {
+    type Error = Error;
+    type Output = Pin<Box<dyn Future<Output = Result<Bytes, Self::Error>> + Send>>;
+    type Builder = RequestBuilder;
+    fn send(&self, request: Request) -> Self::Output {
+      Box::pin(self.execute(request).and_then(|r| r.bytes()))
+    }
+    fn get(&self, url: &str) -> Self::Builder {
+      self.get(url)
+    }
+  }
+  impl super::RequestBuilder for RequestBuilder {
+    type Built = Request;
+    type BuildError = Error;
+    fn add_header(self, name: &str, value: &str) -> Self {
+      self.header(name, value)
+    }
+    fn add_queries<S: Serialize>(self, query: &S) -> Self {
+      self.query(query)
+    }
+    fn build(self) -> Result<Self::Built, Self::BuildError> {
+      self.build()
+    }
+  }
 }
