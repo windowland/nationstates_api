a8aed9ef1fa6042f537539ed4efbc3155d5fcd92
window_land <windowlandns@gmail.com>
2022-01-16 16:49:44 -0600

    added response abstraction

--- src/request.rs
+++ src/request.rs
@@ -10,12 +10,15 @@ use std::future::Future;
 /// This trait is designed for use by http clients.
 pub trait Client {
     ///Error returned if the request fails.
-    type Error: StatusCode;
+    type Error;
+    ///Response type returned by the request
+    type Response: Response;
     ///Future returned by send.
-    type Output: Future<Output = Result<Bytes, Self::Error>> + Unpin + Send;
+    type Output: Future<Output = Result<Self::Response, Self::Error>> + Unpin + Send;
+    ///The request builder type associated with this
     type Builder: RequestBuilder;
     ///Sends the request, returning a future that resolves to the output.
-    /// The server returning a 4XX or a 5XX response code should be considered an error.
+    /// The server returning a 4XX or a 5XX response code should not be considered an error.
     fn send(&self, request: <Self::Builder as RequestBuilder>::Built) -> Self::Output;
     ///Returns a configurable builder that represents a get request to the url.
     /// This function should not panic, instead errors should be reported when
@@ -38,33 +41,39 @@ pub trait RequestBuilder {
     /// Builds the request, returning an error if something went wrong when building.
     fn build(self) -> Result<Self::Built, Self::BuildError>;
 }
-///Trait for fetching an http status code from a type. Used to enable
-/// elegant error handling in the case of user error.
-pub trait StatusCode {
-    ///Gets the status code, if one exists.
-    fn status_code(&self) -> Option<u16>;
-}
 
+///Represents an http reponse from a server.
+pub trait Response {
+    ///Future that will resolve to the request body.
+    type Future: Future<Output = Result<Bytes, Self::Error>> + Unpin + Send;
+    /// Error returned when body can't be read.
+    type Error;
+    ///gets the value of the header represented by name, if one exists.
+    fn get_header(&self, name: &str) -> Option<&[u8]>;
+    ///gets the status code of the response.
+    fn status_code(&self) -> u16;
+
+    ///gets the body of response, consuming self.
+    fn body(self) -> Self::Future;
+}
 #[cfg(feature = "reqwest")]
 mod reqwest {
     use ::reqwest::Client;
     use ::reqwest::Error;
     use ::reqwest::Request;
     use ::reqwest::RequestBuilder;
+    use ::reqwest::Response;
     use bytes::Bytes;
-    use futures::TryFutureExt;
     use serde::Serialize;
     use std::future::Future;
     use std::pin::Pin;
     impl super::Client for Client {
         type Error = Error;
-        type Output = Pin<Box<dyn Future<Output = Result<Bytes, Self::Error>> + Send>>;
+        type Output = Pin<Box<dyn Future<Output = Result<Response, Self::Error>> + Send>>;
         type Builder = RequestBuilder;
+        type Response = Response;
         fn send(&self, request: Request) -> Self::Output {
-            Box::pin(
-                self.execute(request)
-                    .and_then(|r| async { r.error_for_status()?.bytes().await }),
-            )
+            Box::pin(self.execute(request))
         }
         fn get(&self, url: &str) -> Self::Builder {
             self.get(url)
@@ -83,9 +92,17 @@ mod reqwest {
             self.build()
         }
     }
-    impl super::StatusCode for Error {
-        fn status_code(&self) -> Option<u16> {
-            self.status().map(|s| s.as_u16())
+    impl super::Response for Response {
+        type Error = Error;
+        type Future = Pin<Box<dyn Future<Output = Result<Bytes, Error>> + Send>>;
+        fn get_header(&self, name: &str) -> Option<&[u8]> {
+            self.headers().get(name).map(|header| header.as_bytes())
+        }
+        fn status_code(&self) -> u16 {
+            self.status().as_u16()
+        }
+        fn body(self) -> Self::Future {
+            Box::pin(self.bytes())
         }
     }
 }
